d64e97b86aed7a8bc02a1265d7c31942b98f7dec
a-che613 <alexche613@gmail.com>
2023-02-06 13:05:41 +0100

    nice

---
+++ script.js
@@ -0,0 +1,105 @@
+
+let image = document.querySelector('.image');
+let button = document.querySelector('.button');
+let button1 = document.querySelector('.button1');
+let button2 = document.querySelector('.button2');
+let button3 = document.querySelector('.button3');
+let button4 = document.querySelector('.button4');
+let button5 = document.querySelector('.button5');
+let button6 = document.querySelector('.button6');
+let button7 = document.querySelector('.button7');
+let button8 = document.querySelector('.button8');
+let button9 = document.querySelector('.button9');
+let button10 = document.querySelector('.button10');
+let button11 = document.querySelector('.button11');
+let main = document.querySelector('.main');
+let newButton = document.querySelector('.btn-div')
+
+
+button.addEventListener('click', () => {
+  image.setAttribute('src', 'images/wp3850825-4k-pc-wallpapers.jpg')
+  button.style.background = 'red';
+  button1.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none'; 
+})
+button1.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Microsoft windows 3d wallpaper - PixelsTalk_Net.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none'; 
+  button1.style.background = 'green'
+})
+button2.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Pin by SiWahyu🗿 on Minion _ Minions wallpaper, Cute minions wallpaper, Cute minions.jpg')
+  button.style.background = 'none';  button3.style.background = 'none'; button1.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none'; 
+  button2.style.background = 'yellow'
+})
+button3.addEventListener('click', () => {
+  image.setAttribute('src', 'images/HD wallpaper_ Windows 10, windows 10x, windows 11, minimalism, material minimal.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button1.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none';   button3.style.background = 'blue';
+})
+button4.addEventListener('click', () => {
+  image.setAttribute('src', 'images/download (7).jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+   button5.style.background = 'none'; button6.style.background = 'none'; button1.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none';   button4.style.background = 'black'
+})
+button5.addEventListener('click', () => {
+  image.setAttribute('src', 'images/1117426.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button6.style.background = 'none'; button1.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none';   button5.style.background = 'magenta'
+})
+button6.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Windows 11 Wallpapers HD 4K Free Download - PixelsTalk_Net.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button1.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none';   button6.style.background = 'brown';
+})
+button7.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Ultra Hd 4k Wallpaper For Pc - 3840x2160 Wallpaper - teahub_io.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+   button8.style.background = 'none'; button9.style.background = 'none'; button1.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none';   button7.style.background = 'navy'
+})
+button8.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Windows 11.png')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none';  button9.style.background = 'none'; button1.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none';   button8.style.background = 'limegreen'
+})
+button9.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Rick and Morty Windows.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button1.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'none';   button9.style.background = 'cyan'
+})
+button10.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Windows 11 Wallpapers HD 4K Free Download.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+   button11.style.background = 'none';   button10.style.background = 'purple'; button1.style.background = 'none';
+})
+button11.addEventListener('click', () => {
+  image.setAttribute('src', 'images/Fruit Wallpaper_ GLASS FRUIM.jpg')
+  button.style.background = 'none'; button2.style.background = 'none'; button3.style.background = 'none';
+  button4.style.background = 'none'; button5.style.background = 'none'; button6.style.background = 'none';
+  button7.style.background = 'none'; button8.style.background = 'none'; button9.style.background = 'none';
+  button10.style.background = 'none'; button11.style.background = 'brown'; button1.style.background = 'none';
+})
